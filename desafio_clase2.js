05eb47090ec4b86abb2a492d29d46bdd61618db0
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-08-15 22:31:12 -0500

    Description of the activity hands of lab

---
+++ desafio_clase2.js
@@ -0,0 +1,20 @@
+//! DESAFIO ENTREGABLE CLASE #2 
+//EMAScript y ECMAScript avanzado
+//Comision: 58055
+//Estudiante: Nathalia Maya Ramirez
+
+
+
+//Crear clase con el array vacio products.
+
+class ProductManager {
+   constructor() {
+      this.products = [];
+   }
+
+   
+
+
+
+
+}
