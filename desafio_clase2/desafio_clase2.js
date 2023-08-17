af27198abffdcccc392d6f5a9bae3ebb5d44ad56
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-08-16 20:06:09 -0500

    agregar metodo some para recorrer array y verificar que no se repita el code

--- desafio_clase2/desafio_clase2.js
+++ desafio_clase2/desafio_clase2.js
@@ -6,16 +6,24 @@
 //Crear clase con el array vacio products.
 class ProductManager {
   constructor() {
+    //Agregara un producto al array vacio de productos inicial.
     this.products = [];
   }
 
-  //Agregara un producto al array vacio de productos inicial.
+
   addProduct(title, description, price, thumbnail, code, stock) {
+    //validar si el code se repite en el array de products.
+    if(this.products.some(producto => producto.code == code)) {
+      console.log(`El codigo ${code} esta repetido`);
+      return;    
+    }
+
     //Generar id incremental
     const idProducto = this.products.length + 1;
 
     //Propiedades del producto
-    const producto = {
+
+    this.products.push({
       id: idProducto,
       title,
       description,
@@ -23,9 +31,7 @@ class ProductManager {
       thumbnail,
       code,
       stock,
-    };
-
-    this.products.push(producto);
+    });
   }
 
   
@@ -49,11 +55,13 @@ class ProductManager {
 
 const productos = new ProductManager();
 
-
+console.log(productos.getProducts())
 
 productos.addProduct("Bicicleta", "Scott doble suspension", 2500000, "No tiene", "abc", 520);
-productos.addProduct("Bicicleta", "Treck Rin 29", 1500000, "No tiene", "abc4", 20);
-productos.addProduct("Bicicleta", "Treck Rin 29", 1500000, "No tiene", "abcd", 20);
 productos.addProduct("Bicicleta", "Treck Rin 29", 1500000, "No tiene", "abcd", 20);
 
-productos.getProductById(2);
+console.log(productos.getProducts())
+
+productos.addProduct("Bicicleta", "Treck Rin 29", 1500000, "No tiene", "abcd5555", 20);
+
+console.log(productos.getProducts())
