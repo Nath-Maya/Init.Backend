0a3fe571d00184f80fea2ebfbab9c39c3a327d69
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-08-22 20:56:21 -0500

    Actividad

--- clase_4/fsPromesas.js
+++ clase_4/fsPromesas.js
@@ -1,22 +1,38 @@
-const fs = require('fs')
-
-
+const fs = require("fs");
 
 const guardarDate = () => {
-   const fecha = new Date().toString()
-   try {
-      fs.writeFileSync("fecha.txt", fecha);
-      console.log("Escribir fecha");
-   } catch (error) {
-      console.error("No entro la fecha", error)
-      
-   }
-
+  const fecha = new Date().toString();
+  try {
+    fs.writeFileSync("fecha.txt", fecha);
+    console.log("Escribir fecha");
+  } catch (error) {
+    console.error("No entro la fecha", error);
+  }
 };
 
 guardarDate();
 
 
+const leerPackageJson = () => {
+   try {
+      const data = fs.readFileSync("package.json", "utf-8");
+      return data;
+   } catch (error) {
+      console.error("Error de la lectura");
+   }
 
+};
 
+const guardarJson =() => {
+   const data = leerPackageJson()
 
+   try {
+      fs.writeFileSync("data.json", JSON.stringify(data));
+      console.log("Archivo creado correctamente");
+   } catch (error) {
+      console.log("error al crear archivo");
+      
+   }
+};
+
+guardarJson();
