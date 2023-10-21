3a4493f53eb1c3f85ab59f0c8b29ca4dabe0518d
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-10-21 12:17:55 -0500

    Archive handlebars view

---
+++ 19-sessions/src/desafio-login/utils.js
@@ -0,0 +1,7 @@
+import { fileURLtoPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLtoPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default __dirname;
