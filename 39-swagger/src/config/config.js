89dffe681a389dde52fe11c7011928035aebca24
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2024-01-02 21:31:35 -0500

    Documentacion API

---
+++ 39-swagger/src/config/config.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const CONFIG = {
+  PORT: process.env.PORT || "8080",
+  DB_NAME: process.env.DB_NAME || "test",
+  MONGO_USER: process.env.MONGO_USER || "",
+  MONGO_PASS: process.env.MONGO_PASS || "",
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID || "",
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET || "",
+  GITHUB_CALLBACK_URL: process.env.GITHUB_CALLBACK_URL || "",
+  DATASOURCE: process.env.DATASOURCE || "MONGO",
+};
+
+console.log(CONFIG.DB_NAME)
+
+export default CONFIG;
