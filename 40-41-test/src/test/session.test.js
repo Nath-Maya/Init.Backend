6f34ab3ed6265ec6db2dd70571b645e6985ffb79
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2024-01-06 21:19:33 -0500

    estructurando test sessions

--- 40-41-test/src/test/session.test.js
+++ 40-41-test/src/test/session.test.js
@@ -1,3 +1,23 @@
 import mongoose from 'mongoose'
-import User from '../daos/mongo/user.mongo.dao.js'
-import Assert from 'a'
+import UserMongoDao from '../daos/mongo/user.mongo.dao.js'
+import Assert from 'assert'
+import connectMongoDB from '../config/configMongoDB.js'
+import Chai from 'chai'
+import Supertest from 'supertest'
+
+mongoose.connect(config.mongoDB.URL);
+
+const assert = Assert.strict
+const expect = Chai.expect
+const requester = Supertest('http://localhost:3000')
+
+describe('Testing User DAO', () => {
+   before(function () {
+      this.usersDAO = new UserMongoDao()
+   
+   })
+
+
+
+
+})
